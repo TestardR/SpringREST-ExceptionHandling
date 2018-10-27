790c83d00c39775559931301d1c3adf4e7d050a9
Romain Testard <40339476+TestardR@users.noreply.github.com>
2018-10-27 15:23:12 +0200

    Add files via upload

---
+++ src/main/java/com/romain/springdemo/rest/StudentRestController.java
@@ -0,0 +1,56 @@
+package com.romain.springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.romain.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+	
+	private List<Student> theStudents;
+	
+	// define @PostCOnstruct to load the student data once
+	
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
+		theStudents.add(new Student("Romain", "Testard"));
+		theStudents.add(new Student("Vanessa", "Renaud"));
+		theStudents.add(new Student("Marie", "Tan"));
+	}
+	
+	// define endpoint for "/students" - return list of students
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		
+		return theStudents;
+		
+	}
+	
+	// define endpoint for "/students/{studentId}" - return student at index
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		// just index into the list
+		// check the studentId against list size
+		
+		if ((studentId >= theStudents.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		return theStudents.get(studentId);
+	}
+	
+	
+}
